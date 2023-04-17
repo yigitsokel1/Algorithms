8ffaa6b7082c239cfa62071185e09abbaa79581a
Osman Yiğit Sökel <108831247+yigitsokel1@users.noreply.github.com>
2023-04-17 15:21:39 +0300

    Add files via upload

---
+++ Searching and Sorting Algorithms/Searching Algorithms/Problems/Two Elements Whose Sum is Closest to Zero.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<limits.h>
+
+void sumClosestZero(int n, int array[]);
+
+
+int main(){
+	
+	srand(time(NULL));
+	int n = 5 + rand() % 5;
+	int arr[n];
+	int i;
+	
+	for(i=0; i<n; i++)
+		arr[i] = 100 - rand() % 200;
+		
+	printf("Array\n");
+	for(i=0; i<n; i++)
+		printf("%d  ", arr[i]);
+	
+	sumClosestZero(n, arr);
+	
+}
+
+void sumClosestZero(int n, int array[]){
+	
+	int i, j;
+	int minEl1, minEl2, minSum = INT_MAX;
+	
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(abs(array[i] + array[j]) < minSum){
+				minEl1 = array[i];
+				minEl2 = array[j];
+				minSum = abs(array[i] +array[j]);
+			}
+		}
+	}
+	
+	printf("\nThe two elements whose sum is minimum are %d and %d, sum: %d",minEl1, minEl2, minSum);
+	
+	
+}
